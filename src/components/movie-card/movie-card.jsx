f7944910148b4c47c606700a07cff83fb4ded2ec
S-Adil <sana.adil.ak@gmail.com>
2022-04-29 02:00:15 -0600

    added PropTypes to these components

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 
 export class MovieCard extends React.Component {
   render() {
     const { movieData, onMovieClick } = this.props;
-    return <div className="movie-card" onClick={() => { onMovieClick(movieData); }} >{movieData.Title}</div>;
+
+    return (
+      <div className="movie-card" onClick={() => { onMovieClick(movieData); }} >{movieData.Title}</div>
+    );
   }
 }
+
+MovieCard.PropTypes = {
+  /*
+  1)The props object must include a movie object (shape({...}) means that it’s an object).
+
+  2)The movie prop (object) may contain a Title key; if it does, then it must be of type string.
+
+  3)The props object must contain onMovieClick and it must be a function */
+
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImageURL: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+    })
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieView extends React.Component {
   render() {
@@ -26,3 +27,22 @@ export class MovieView extends React.Component {
     );
   }
 }
+
+MovieView.PropTypes = {
+  /*
+  1)The props object must include a movie object (shape({...}) means that it’s an object).
+
+  2)The movie prop (object) may contain a Title key; if it does, then it must be of type string.
+
+  3)The props object must contain onMovieClick and it must be a function */
+
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImageURL: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+    })
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};

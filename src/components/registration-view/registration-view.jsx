ecf1b31c57d69c423cfc53eb82711365f924f598
S-Adil <sana.adil.ak@gmail.com>
2022-04-29 01:58:26 -0600

    create registration-view component
    used useState React hook to create registration-view component

---
+++ src/components/registration-view/registration-view.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+export function RegistrationView(props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [birthday, setBirthday] = useState('');
+  // call useState() with empty string b/c this is the initial value of your login variable
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password, email, birthday);
+    /* Send a request to the server for authentication */
+    /* then call props.onLoggedIn(username) */
+    props.onLoggedIn(username);
+
+  };
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+      </label>
+      <label>
+        Birthday:
+        <input type="birthday" value={birthday} onChange={e => setEmail(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Register</button>
+    </form>
+  );
+}

f40fb1b20f6ef977366e707733890eb03495088b
S-Adil <sana.adil.ak@gmail.com>
2022-04-30 18:47:20 -0600

    imported login-view component

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,27 +7,21 @@ import React from 'react';
 //extend keyword for extending from React.Component, create MainView comp using generic React.Comp template as a foundation
 import axios from 'axios';
 
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
 
-  // render function returns visual representation of the component (what will be displayed on the screen)
-  // the inside looks like HTML but it's JSX
-  //render fcn can only have one root element
-
-  //adding a movies state that will hold the list of movies. MainView state is initialized with an object containing movies implemented by this.state inside a method called constructor
-  //constructor method is used to create component. code inside it will be the first thing to be executed for a component
-  //constructor() method is executed before the render() method. This is why constructor is the place to initialize a state’s values.
-
-
   constructor() {
     //super(); initializes your component’s state, and without it, you’ll get an error if you try to use this.state inside constructor(). Can omit if you don't use this.state
     super();
+    // Initial state is set to null
     this.state = {
       movies: [],
-      selectedMovie: null
-    }
+      selectedMovie: null,
+      user: null
+    };
   }
 
 
@@ -54,23 +48,36 @@ export class MainView extends React.Component {
     // code executed just before the moment the component gets removed from the DOM.
   }
 
-
+  /*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie*/
   setSelectedMovie(newSelectedMovie) {
     this.setState({
       selectedMovie: newSelectedMovie
     });
   }
 
+  /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
+  onLoggedIn(user) {
+    this.setState({
+      user
+    });
+  }
+
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, user } = this.state;
 
+    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView */
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
+    // Before the movies have been loaded
     if (movies.length === 0)
       return <div className="main-view" />;
 
-    return ( //written with ternary operator for if else in JS, thats what the ? and : are for
+    return (
       <div className="main-view">
+        {/* If the state of 'selectedMovie' is not null, that selected movie will be returned otherwise, all "movies will be returned */}
         {selectedMovie
+          //written with ternary operator for if else in JS, thats what the ? and : are for
+
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
           : movies.map(movie => (
             <MovieCard key={movie._id} movieData={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
